fb9c7115ee8de529d6f77f740060a1ed22aa99c9
Victor Fernandes <victorfernandes91@hotmail.com>
2019-05-22 22:53:22 -0300

    added new layouts

--- app/src/main/java/com/voidx/github/feature/user/list/view/ListUserFragment.kt
+++ app/src/main/java/com/voidx/github/feature/user/list/view/ListUserFragment.kt
@@ -1,32 +1,69 @@
 package com.voidx.github.feature.user.list.view
 
-
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import com.voidx.github.R
+import com.voidx.github.feature.user.list.UserListContract
+import org.koin.android.ext.android.inject
+import org.koin.core.parameter.parametersOf
+import kotlinx.android.synthetic.main.fragment_list_user.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class ListUserFragment : Fragment(), UserListContract.View {
 
-/**
- * A simple [Fragment] subclass.
- *
- */
-class ListUserFragment : Fragment() {
+    val presenter : UserListContract.Presenter by inject { parametersOf(this) }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    val adapter: ListUserAdapter by inject()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_list_user, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        this.users.adapter = this.adapter
+    }
 
+    override fun onStart() {
+        super.onStart()
+        presenter.load()
+    }
+
+    override fun showLoading() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun hideLoading() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun showUsers() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun hideUsers() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun showUser(name: String) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun showError() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun hideError() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun showEmptyError() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun hideEmpty() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
